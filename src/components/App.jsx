bc75670eb8865b72d6eb70149e052829a5c5fdb0
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-22 21:49:13 +0300

    numbers added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
+import { ContactBook } from './ContactBook/ContactBook';
+import Container from './Container/Container';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Container><ContactBook /></Container>
 };
---
+++ src/components/ContactBook/ContactBook.jsx
@@ -0,0 +1,67 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import styled from 'styled-components';
+import { nanoid } from 'nanoid'
+
+const NAME_INPUT_TITLE = "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan";
+const NAME_INPUT_PATTERN = "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$";
+const NUMBER_INPUT_TITLE = "Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+
+const ErrorText = styled.p`
+  color: red;
+`;
+
+const FormError = ({ name }) => {
+  return (
+    <ErrorMessage
+      name={name}
+      render={message => <ErrorText>{message}</ErrorText>}
+    />
+  );
+};
+
+const validationSchema = Yup.object({
+  name: Yup.string().required(),
+  number: Yup.number().required(),
+});
+
+const initialValues = {
+    contacts: [],
+    name: '',
+    number: ''
+};
+
+export const ContactBook = () => {
+    const handleSubmit = (values, { resetForm }) => {
+    values.id = nanoid();
+    console.log(values);
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form autoComplete="off">
+        <div>
+          <label htmlFor="name">Name</label>
+          <div>
+            <Field name="name" type="text" placeholder="Full name" pattern={NAME_INPUT_PATTERN} title={NAME_INPUT_TITLE} />
+            <FormError name="name" />
+          </div>
+        </div>
+        <div>
+          <label htmlFor="number">Number</label>
+          <div>
+            <Field name="number" type="tel" placeholder="Number" pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}" title={NUMBER_INPUT_TITLE} />
+            <FormError name="number" />
+          </div>
+        </div>
+        
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
+};
