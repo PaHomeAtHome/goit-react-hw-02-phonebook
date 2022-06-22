38e3557dce752527d9f9730ff2f0249e7fb52ba3
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-23 02:21:13 +0300

    it works)

--- src/components/ContactBook/ContactBook.jsx
+++ src/components/ContactBook/ContactBook.jsx
@@ -1,4 +1,4 @@
-import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import styled from 'styled-components';
 import { nanoid } from 'nanoid'
@@ -10,6 +10,7 @@ const NUMBER_INPUT_TITLE = "Phone number must be digits and can contain spaces, 
 const ErrorText = styled.p`
   color: red;
 `;
+
 const FormError = ({ name }) => {
   return (
     <ErrorMessage
@@ -31,44 +32,51 @@ const initialValues = {
 };
 
 export const ContactBook = () => {
-    const handleSubmit = (values, { resetForm }, push) => {
-    values.id = nanoid();
+    const handleSubmit = (values, { resetForm }) => {
         const { name, number } = values;
         const contact = {
-           [name]: number,
+            name: [name],
+            number: [number],
+            id: nanoid()
         }
         values.contacts.push(contact)
         console.log(values.contacts)
 
         resetForm();
-  };
+    }
 
-    return (
-    <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-            enableReinitialize
-    >
-            <Form autoComplete="off">
-                <h2>Phonebook</h2>
-        <div>
-          <label htmlFor="name">Name</label>
-          <div>
-            <Field name="name" type="text" placeholder="Full name" pattern={NAME_INPUT_PATTERN} title={NAME_INPUT_TITLE} />
-            <FormError name="name" />
-          </div>
-        </div>
-        <div>
-          <label htmlFor="number">Number</label>
-          <div>
-            <Field name="number" type="tel" placeholder="Number" pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}" title={NUMBER_INPUT_TITLE} />
-            <FormError name="number" />
-          </div>
-        </div>
-                <button type="submit">Add contact</button> 
-                <h2>Contacts</h2>
-            </Form>
-        </Formik>
-    );
-};
+        return (
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+               
+            >
+                {({ values }) => (
+                <Form autoComplete="off">
+                    <h2>Phonebook</h2>
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <div>
+                            <Field name="name" type="text" placeholder="Full name" pattern={NAME_INPUT_PATTERN} title={NAME_INPUT_TITLE} />
+                            <FormError name="name" />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="number">Number</label>
+                        <div>
+                            <Field name="number" type="tel" placeholder="Number" pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}" title={NUMBER_INPUT_TITLE} />
+                            <FormError name="number" />
+                        </div>
+                    </div>
+                    <button type="submit">Add contact</button>
+                    <h2>Contacts</h2>
+                        {values.contacts.map(contact => {
+                            return <p key={contact.id}>{contact.name}: {contact.number} ({contact.id})</p>
+                        })
+                        }
+                
+                </Form>)}
+            </Formik>
+        );
+    };
