db6e1d9efa3a77fa8bf8a2a3c3c60a440b375213
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-24 01:49:10 +0300

    some fixes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,24 +1,13 @@
-import styled from 'styled-components';
-
-const Element = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-`
-
-const Button = styled.button`
-margin-left: 10px;
-color: white;
-background-color: #CC000099;
-cursor: pointer;
-border: 1px solid grey;
-border-radius: 5px;`
+import { ContactListComponent } from 'components/ContactListComponent/ContactListComponent';
 
 export const ContactList = ({ values, deleteContact }) => 
-    
-    values.filter === '' ? values.contacts.map(contact => <Element key={contact.id}><p>{contact.name}: {contact.number}</p>
-        <Button type="button" onClick={() => deleteContact(contact.id)} >Delete</Button></Element>)
+    <ul>
+        
+        {values.filter === '' ? values.contacts.map(contact => <ContactListComponent key={contact.id} contact={contact} deleteContact={deleteContact} />)
                             
-    : values.contacts.filter(contact => contact.name.toLowerCase().includes(values.filter.toLowerCase()))
-        .map(contact => <Element key={contact.id}><p>{contact.name}: {contact.number}</p>
-        <Button type="button" onClick={() => deleteContact(contact.id)} >Delete</Button></Element>)
+            : values.contacts.filter(contact => contact.name.toLowerCase().includes(values.filter.toLowerCase()))
+                .map(contact => <ContactListComponent key={contact.id} contact={contact} deleteContact={deleteContact}/>)}
+
+    </ul>
+    
+    
